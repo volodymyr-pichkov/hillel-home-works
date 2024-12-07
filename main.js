d7ddefdab670a8abe2a0d25006966b65bffd2d5d
[Volodymyr Pichkov] <[zelastar4872@gmail.com]>
2024-12-07 09:44:32 +0200

    delete

--- main.js
+++
@@ -1,5 +0,0 @@
-import { getData } from "./js/getData.js";
-import { postData } from "./js/postData.js";
-import { putData } from "./js/putData.js";
-import { patchData } from "./js/patchData.js";
-import { deleteData } from "./js/deleteData.js";
