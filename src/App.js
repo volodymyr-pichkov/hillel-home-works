f1e4a4aa54b026c10b09425817862b4072da6e58
volodymyr-pichkov <volodymyr2000pichkov@gmail.com>
2025-01-08 20:37:15 +0200

    hw39

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Greeting from './components/Greeting.jsx';
+import Counter from './components/Counter.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <Greeting name="Алексей" />
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Greeting.jsx
@@ -0,0 +1,5 @@
+const Greeting = ({ name }) => {
+    return <h2>Привет, {name}!</h2>;
+};
+
+export default Greeting;
