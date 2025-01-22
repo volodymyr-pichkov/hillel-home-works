858d8795a86b43ddc41b89f1c0c7893423d0090b
volodymyr-pichkov <volodymyr2000pichkov@gmail.com>
2025-01-22 15:11:18 +0200

    delete

--- src/components/tailwind-test.jsx
+++
@@ -1,9 +0,0 @@
-const TailwindTest = () => {
-  return (
-    <h1 className="text-3xl font-extrabold text-9xl text-purple-500 italic">
-      Wazzup Beijing
-    </h1>
-  );
-};
-
-export default TailwindTest;
