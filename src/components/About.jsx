ea31a1aeb2be232282233de902383002292b3320
volodymyr-pichkov <volodymyr2000pichkov@gmail.com>
2025-01-23 17:51:17 +0200

    hw 42

---
+++ src/components/About.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const About = () => {
+  return <h1 className="flex justify-center text-4xl text-yellow-500 mt-5">О нас</h1>;
+};
+
+export default About;
---
+++ src/components/Home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <h1 className="flex justify-center text-4xl text-red-500 mt-5">Домашняя страница</h1>;
+};
+
+export default Home;
